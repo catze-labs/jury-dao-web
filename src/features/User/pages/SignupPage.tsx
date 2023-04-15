37545854024c5676f5bef3e3ef4598d9335c152e
JungAh Choi (최정아) <65520261+jamie-jungah@users.noreply.github.com>
2023-04-15 11:22:06 +0900

    Add Sign up UI (#19)

---
+++ src/features/User/pages/SignupPage.tsx
@@ -0,0 +1,38 @@
+import { NextPage } from "next";
+import { useState } from "react";
+import ConnectWallet from "../components/ConnectWallet";
+import SignupForm from "../components/signup/SignupForm";
+import PhaseCircle from "../components/signup/PhaseCircle";
+import Signing from "../components/Signing";
+import SignError from "../components/SignError";
+
+const Phase = {
+  Wallet: 0,
+  Signing: 1,
+  Info: 2,
+}
+
+const SignupPage: NextPage = () => {
+  const [phase, setPhase] = useState<number>(0);
+
+  return (
+    <div className="w-full h-[120vh] bg-primary-300 flex justify-center items-center">
+      <div className="w-[412px] h-[490px] flex flex-col">
+        <div className="flex flex-col gap-10">
+          <h1 className="text-white font-bold text-[32px] leading-[48px] text-center">Sign up</h1>
+          <PhaseCircle phase={phase} onClick={setPhase} />
+        </div>
+
+        <form
+          className="flex justify-center items-center"
+        >
+          {phase === Phase.Wallet && <ConnectWallet />}
+          {phase === Phase.Signing && <Signing />}
+          {phase === Phase.Info && <SignupForm />}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
